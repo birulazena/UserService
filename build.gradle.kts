36cb55a9d738eddf4c02ae02e09b64f01348bf2f
birulya.zhenka <birulya.zhenka@gmail.com>
2026-01-13 21:16:17 +0300

    feat: complete DBSchema task

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-liquibase")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.postgresql:postgresql:42.7.3")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
