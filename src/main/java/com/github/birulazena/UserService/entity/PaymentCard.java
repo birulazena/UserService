1d15cb1df8d245e1df6f7258e640783cc59a381e
birulya.zhenka <birulya.zhenka@gmail.com>
2026-01-14 18:32:27 +0300

    feat: complete  DAO Layer task

---
+++ src/main/java/com/github/birulazena/UserService/entity/PaymentCard.java
@@ -0,0 +1,36 @@
+package com.github.birulazena.UserService.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "payment_cards")
+@ToString(exclude = "user")
+public class PaymentCard extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String number;
+
+    @Column(nullable = false)
+    private String holder;
+
+    @Column(name = "expiration_date", nullable = false)
+    private LocalDate expirationDate;
+
+    @Column(nullable = false)
+    private Boolean active;
+}
